27859bf6103d72c624f43f8225dd021cb99687a8
汪巧娟 <qj.wang@betterwood.com>
2021-08-13 16:16:03 +0800

    test(prettiereslinttest): 增加eslint配置测试

---
+++ src/components/PrettierEslintTest/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function PrettierEslintTest() {
+  return (
+    <div>
+      <h1>PrettierEslintTest组件</h1>
+      <span>"啦啦啦啦"</span>
+      <span>'啦啦啦啦'</span>
+    </div>
+  )
+}
--- src/pages/Test/index.tsx
+++ src/pages/Test/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 // import TestCom from '../../components/TestCom'
+import PrettierEslintTest from '../../components/PrettierEslintTest'
 const Test = () => {
   return (
     <div>
       <h2>Test page</h2>
       {/* <TestCom /> */}
+      <PrettierEslintTest />
     </div>
   )
 }
